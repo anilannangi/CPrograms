25fe7666ae8bc06e1bea86dccc5a854b595f7a64
lenovo <anilannangi25@gmail.com>
2021-11-13 09:31:13 +0530

    malll

--- Union.c
+++ Union.c
@@ -7,15 +7,15 @@ union Student
 }
 int main()
 {
-  union Student s1;
-  printf("enter no");
-  scanf("%d",&s1.no);
-  printf("enter name");
-  while(getchar()!='\n');
-  gets_s(&s1.name);
-  printf("enter height");
-  scanf("%f",&s1.height);
-  printf("number=%d\n name=%c\n height=%f\n, s1.no,s1.name,s1.height);
-         return 0;
+    union Student s1;
+    printf("enter no");
+    scanf("%d", &s1.no);
+    printf("enter name");
+    while (getchar() != '\n');
+    gets_s(&s1.name);
+    printf("enter height");
+    scanf("%f", &s1.height);
+    printf("number=%d\n name=%c\n height=%f\n, s1.no,s1.name,s1.height);
 }
-  
+
+//dsfdsfsfsdfsdfsf
